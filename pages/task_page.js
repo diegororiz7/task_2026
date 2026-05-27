91ef2f3b674e2136d815877e9dbc6254b00de460
diegororiz7 <diegororiz7@gmail.com>
2026-05-26 21:39:56 -0300

    Aula 26-05-2026

--- pages/task_page.js
+++ pages/task_page.js
@@ -8,6 +8,7 @@ import {Picker} from '@react-native-picker/picker';
 import { FontAwesome } from '@expo/vector-icons';
 import styles from '../styles/styles_task';
 
+
 //Função principal (Listar, ordenar, buscar e manipular)
 export default function Task({navigation}){
 
@@ -31,6 +32,16 @@ export default function Task({navigation}){
     function confirmDeleteTask(id){
         setTaskToDelete(id);
         setShowDeleteModal(true);
+    }
+
+    //Função para excluir a tarefa
+    function deleteTask(){
+        if(taskToDelete){
+            database.collection('Tasks').doc(taskToDelete).delete();
+            setTask(prevTask => prevTask.filter(
+                task => task.id !== taskToDelete
+            ))
+        }
     }
 
     //Função para cancelar a exclusão da tarefa
@@ -94,6 +105,7 @@ export default function Task({navigation}){
     useEffect(() => {
         const unsubscribe = database.collection('Tasks')
                             .onSnapshot((QuerySnapshot) => {
+                                const list = [];
                                 QuerySnapshot.forEach((doc) => {
                                     list.push({...doc.data(), id: doc.id})
                                 });
@@ -108,7 +120,7 @@ export default function Task({navigation}){
             const matchStatus = hideFinished ?
             !t.finished : true;
             const matchSearch = t.description.toLowerCase().includes(
-                searchText.toLowerCase
+                searchText.toLowerCase()
             );
             return matchSearch && matchStatus;
         });
@@ -137,6 +149,199 @@ export default function Task({navigation}){
                     Ocultar tarefas finalizadas
                 </Text>
             </View>
+
+            {/*Selecionar tipo de ordenação*/}
+            <View style = {{
+                flexDirection: 'row',
+                justifyContent: 'flex-end',
+                alignItems: 'center',
+                marginTop: 15,
+                marginBottom: 10,
+                paddingRight: 10
+            }}>
+                <Text style = {{fontWeight: 'bold', marginRight: 10}}>
+                    Ordenar por:
+                </Text>
+                <Picker
+                    style = {{width: 150}}
+                    value = {sortOption}
+                    onValueChange={(itemValue) => setSortOption(itemValue)}
+                >
+                    <Picker.Item label = 'Data' value = 'date'/>
+                    <Picker.Item label = 'Prioridade' value = 'priority'/>
+                </Picker>
+            </View>
+
+            {/*Buscar tarefa */}
+            <View style = {{
+                flexDirection: 'row',
+                alignItems: 'center',
+                backgroundColor: '#f0f0f0',
+                borderRadius: 8,
+                marginHorizontal: 10,
+                marginBottom: 10,
+                paddingHorizontal: 10
+            }}>
+                <FontAwesome name = 'search' size = {18} color = '#000'/>
+                <TextInput
+                    value = {searchText}
+                    onChangeText = {setSearchText}
+                    style = {styles.searchInput}
+                    placeholder = 'Pesquise uma tarefa'
+                />
+            </View>
+
+            {/*Lista de Tarefas*/}
+            <FlatList
+                data = {filteredTask}
+                keyExtractor = {(item) => item.id}
+                renderItem = {({item}) => (
+                    <View style = {styles.contextAllTasks}>
+                        <View style = {{
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                            gap: 6
+                        }}>
+                            <Text style = {[
+                                styles.desciptionTask,
+                                item.finished && {
+                                    textDecorationLine: 'line-through'
+                                }
+                            ]}>
+                                {item.description}
+                            </Text>
+
+                            <Text style = {{
+                                backgroundColor: getPriorityColor(item.priority),
+                                color: '#fff',
+                                paddingHorizontal: 6,
+                                borderRadius: 4
+                            }}>
+                                {item.priority}
+                            </Text>
+
+                            <Text>
+                                {item.date ? 
+                                    formatDate(item.date) : ''
+                                }
+                            </Text>
+                        </View>
+
+                        {/*Botões de ações das tarefas */}
+                        <View style = {{
+                            flexDirection: 'row'
+                        }}>
+                            {/*Botão de curtir*/}
+                            <TouchableOpacity
+                                style = {{marginRight: 10, alignItems: 'center'}}
+                                onPress={() => handleLike(item.id, currentLikes)}
+                            >
+                                <FontAwesome
+                                    name = 'thumbs-up'
+                                    size = {20}
+                                    color = '#007BFF'
+                                />
+                            </TouchableOpacity>
+                            
+                            {/*Botão de descurtir*/}
+                            <TouchableOpacity
+                                style = {{marginRight: 10, alignItems: 'center'}}
+                                onPress={() => handleDeslike(item.id, currentDeslikes)}
+                            >
+                                <FontAwesome
+                                    name = 'thumbs-down'
+                                    size = {20}
+                                    color = '#FF0000'
+                                />
+                            </TouchableOpacity>
+
+                            {/*Botão de finalizar tarefa*/}
+                            <TouchableOpacity
+                                style = {{marginRight: 10, alignItems: 'center'}}
+                                onPress={() => toggleTaskStatus(item.id, item.finished)}
+                            >
+                                <FontAwesome
+                                    name = {item.finished ? 'undo' : 'check'}
+                                    size = {20}
+                                    color = '#4CAF50'
+                                />
+                            </TouchableOpacity>
+
+                            {/*Botão de editar tarefa*/}
+                            <TouchableOpacity
+                                style = {{marginRight: 10, alignItems: 'center'}}
+                                onPress={()=> navigation.navigate('AddEdit',{
+                                    description: item.description,
+                                    priority: item.priority,
+                                    date: item.date
+                                })}
+                            >
+                                <FontAwesome
+                                    name = 'edit'
+                                    size = {20}
+                                    color = '#9C27B0'
+                                />
+                            </TouchableOpacity>
+
+                            {/*Botão de excluir tarefa*/}
+                            <TouchableOpacity
+                                style = {{marginRight: 10, alignItems: 'center'}}
+                                onPress={()=> confirmDeleteTask(item.id)}
+                            >
+                                <FontAwesome
+                                    name = 'trash'
+                                    size = {20}
+                                    color = '#D32F2F'
+                                />
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                )}
+            />
+
+            {/*Botão de adicionar tarefa*/}
+            <TouchableOpacity
+                style = {styles.buttonNewTask}
+                onPress = {()=> navigation.navigate('AddEdit')}
+            >
+                <FontAwesome
+                    name = 'plus'
+                    size = {20}
+                    color = '#FFF'
+                />
+            </TouchableOpacity>
+
+            {/*Modal de exclusão*/}
+            <Modal
+                visible = {showDeleteModal}
+                transparent
+            >
+                <View style = {styles.overlay}>
+                    <View style = {styles.modalContent}>
+                        <Text>Deseja realmente excluir a tarefa?</Text>
+                        <View style = {styles.buttonContainer}>
+                            <TouchableOpacity
+                                style = {[styles.button, {
+                                    backgroundColor: '#4CAF50',
+                                    width: '45%'
+                                }]}
+                                onPress = {cancelDelete}
+                            >
+                                <Text style = {styles.buttonText}>Cancelar</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                style = {[styles.button, {
+                                    backgroundColor: '#D32F2F',
+                                    width: '45%'
+                                }]}
+                                onPress = {deleteTask}
+                            >
+                                <Text style = {styles.buttonText}>Confirmar</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+            </Modal>
         </View>
     );
 }
